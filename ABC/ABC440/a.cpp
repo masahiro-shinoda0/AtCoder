1c8e1f6ef4011b33b36ff5f4c0eebc0edb4f14ab
MasahiroShinoda <masahiro2019@docomo.ne.jp>
2026-01-10 21:50:01 +0900

    Update a.cpp

--- ABC/ABC440/a.cpp
+++ ABC/ABC440/a.cpp
@@ -1,18 +1,1 @@
 #include <iostream>
-using namespace std;
-
-int main(){
-    int x, y;
-    cin >> x >> y;
-
-    int ans = 1;
-    ans = x;
-
-    for(int i=0; i<y; i++){
-        ans *= 2;
-    }
-
-    cout << ans << endl;
-
-    return 0;
-}
