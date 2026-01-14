6a89c63c86b726fbc4cfaf9ce99ac610d4ff92ac
masahiro-shinoda0 <masahiro192489@gmail.com>
2026-01-14 23:30:06 +0900

    編集

---
+++ cpp/2_math/math.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "input n: ";
+    cin >> n;
+
+    cout << "pow(n,2) = " << pow(n,2) << endl;
+    cout << "sqrt(n,2) = " << sqrt(n) << endl;
+    cout << "abs(n) = " << abs(n) << endl;
+    cout << "sin(n) = " << sin(n) << endl;
+    cout << "cos(n) = " << cos(n) << endl;
+    cout << "tan(n) = " << tan(n) << endl;
+    cout << "exp(n) = " << exp(n) << endl;
+    cout << "exp2(n) = " << exp2(n) << endl;
+    cout << "log(n) = " << log(n) << endl;
+    cout << "log10(n) = " << log10(n) << endl;
+
+    return 0;
+}
