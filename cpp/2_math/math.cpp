f07855a50c79800217d069ca1733c8032797d1e2
masahiro-shinoda0 <masahiro192489@gmail.com>
2026-01-14 23:50:09 +0900

    math

--- cpp/2_math/math.cpp
+++ cpp/2_math/math.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <cmath>
+#include <limits> // エラー処理
 using namespace std;
 
 int main()
 {
-    int n;
-    cout << "input n: ";
-    cin >> n;
+    double n;
 
-    cout << "pow(n,2) = " << pow(n,2) << endl;
-    cout << "sqrt(n,2) = " << sqrt(n) << endl;
-    cout << "abs(n) = " << abs(n) << endl;
-    cout << "sin(n) = " << sin(n) << endl;
-    cout << "cos(n) = " << cos(n) << endl;
-    cout << "tan(n) = " << tan(n) << endl;
-    cout << "exp(n) = " << exp(n) << endl;
-    cout << "exp2(n) = " << exp2(n) << endl;
-    cout << "log(n) = " << log(n) << endl;
-    cout << "log10(n) = " << log10(n) << endl;
+    while(true){
+        cout << "input n: ";
+        cin >> n;
+        if(!n){
+            cout << "n is NaN" << endl;
+            cin.clear(); // リセット
+            // バッファにある不正な文字を最後まで読み飛ばす
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        cout << "pow(n,2) = " << pow(n,2) << endl;
+        cout << "sqrt(n,2) = " << sqrt(n) << endl;
+        cout << "abs(n) = " << abs(n) << endl;
+        cout << "sin(n) = " << sin(n) << endl;
+        cout << "cos(n) = " << cos(n) << endl;
+        cout << "tan(n) = " << tan(n) << endl;
+        cout << "exp(n) = " << exp(n) << endl;
+        cout << "exp2(n) = " << exp2(n) << endl;
+        cout << "log(n) = " << log(n) << endl;
+        cout << "log10(n) = " << log10(n) << endl;
+
+        cout << endl;
+        cout << "continue?[y/n]" << endl;
+        string s;
+        cin >> s;
+        if(s=="n" || s=="N") break;
+        cout << endl;
+    }
 
     return 0;
 }
